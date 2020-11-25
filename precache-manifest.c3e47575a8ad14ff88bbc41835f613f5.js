11f47af5b2db1264708c6a14261d6ae136f1e708
Jesus-Salvador-Beltran-Perez <Jesus.Salvador.Beltran.Perez@ibm.com>
2020-11-25 16:07:44 -0700

    Updates

---
+++ precache-manifest.c3e47575a8ad14ff88bbc41835f613f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f3b39bf087ef49e53f2f711fd662ca",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "edf5c84c8607ed12d2d0",
+    "url": "/monstersrolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monstersrolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf5c84c8607ed12d2d0",
+    "url": "/monstersrolodex/static/js/main.694bec1e.chunk.js"
+  },
+  {
+    "revision": "902caa65de6c41620983",
+    "url": "/monstersrolodex/static/js/runtime-main.cc379c08.js"
+  }
+]);
